9d21396784443ba56d5eb9cef90f124ea95efc44
micaelacrocco <micaela.crocco@correo.ucu.edu.uy>
2024-09-23 15:49:43 -0300

    todolist

--- todolist-app/src/TodoList.js
+++ todolist-app/src/TodoList.js
@@ -1,8 +1,41 @@
 import React, {useState} from 'react';
-import './Todolist.css';
 
-const TodoList = () => {
-    
-}
+function TodoList() {
+    const [task, setTask] = useState(''); 
+    const [tasks, setTasks] = useState([]); 
+
+    const handleAddTask = () => {
+        if (task.trim()) {
+            setTasks([...tasks, task]); 
+            setTask(''); 
+        }
+    };
+
+    const handleDeleteTask = (index) => {
+        const newTasks = tasks.filter((_, i) => i !== index); 
+        setTasks(newTasks);
+    };
+
+    return (
+        <div>
+            <h2>Todo List</h2>
+            <input
+            type="text"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+            placeholder="Escribe una tarea"
+            />
+            <button onClick={handleAddTask}>Agregar Tarea</button>
+            <ul>
+            {tasks.map((task, index) => (
+                <li key={index}>
+                {task}
+                <button onClick={() => handleDeleteTask(index)}>Eliminar</button>
+                </li>
+            ))}
+                </ul>
+        </div>
+    );
+    }
 
 export default TodoList;
